9d5bfa881c450439803d8e991ccfaae65e426602
amsatique <amsatique@gmail.com>
2016-05-02 08:16:20 +0200

    change loop

--- test.sh
+++ test.sh
@@ -4,50 +4,61 @@
 rm -rf /private/docker/complete/*
 rm -rf /private/docker/incomplete/*
 rm -rf /private/docker/progress/*
+rm -rf /private/docker/csv*
 
 #Set var
 DATE='/bin/date'
-ramlimit=( 512m 1024m 2048m nolimit )
-input=( big_buck_bunny_720p_10mb.mp4 big_buck_bunny_720p_50mb.mp4 big_buck_bunny_720p_10mb.flv big_buck_bunny_480p_50mb.flv big_buck_bunny_480p_10mb.mkv big_buck_bunny_480p_50mb.mkv )
-input_format=$(echo $input | cut -d '.' -f2)
+ramlimit=( nolimit ) #512m 1024m 2048m )
 name=$(echo $input | cut -d '.' -f1)
-extensions=( MP4 MOV AVI WMV MKV SWF ASF FLV VOB RM 3GP WEBM MPG DV M4A M4R MP3 WAV FLAC WMA AC3 AAC OGG RA )
+extensions=( MP4 MOV AVI WMV MKV SWF ASF FLV VOB RM 3GP WEBM MPG MP3 WAV FLAC WMA AC3 AAC OGG RA )
 
-#Loop for docker options
-for l in "${ramlimit[@]}"
+echo "X" >> /private/docker/csv
+#Shape table
+for e in ${extensions[@]}
 do
-  #Loop for multiple input
-  for n in "${input[@]}"
-  do
+  echo $e >> /private/docker/csv
+done
+e=$(wc -l /private/docker/csv)
+e=$(echo $e | cut -d ' ' -f1)
+
+
+#Loop for multiple input
+for r in "${ramlimit[@]}"
+do
   for i in ${extensions[@]}
   do
-    customer="$i-$(echo ${n} | cut -d '_' -f5 | cut -d '.' -f1)-$l"
-        for j in ${extensions[@]}
-        do
-                if [ $i != $j ]
-                then
-              echo "Test for ${m}"
-              sleep 3
-              cp /private/docker/${n} /private/docker/incomplete/
-              BEFORE=$($DATE +'%s')
-              if [ $l != "nolimit" ]
-              then
-                sudo docker run -it --rm -e INPUT=${n} -m ${l} -e CUSTOMER=${customer} -e EXTENSION=${j} -v /private/docker:/data transcode
-              else
-                sudo docker run -it --rm -e INPUT=${n} -e CUSTOMER=${customer} -e EXTENSION=${j} -v /private/docker:/data transcode
-              fi
-              AFTER=$($DATE +'%s')
-              ELAPSED=$(($AFTER - $BEFORE))
-              if [ $ELAPSED -lt 3 ]
-              then
-                rm /private/docker/complete/${customer}/$name.${m}
-                echo "Format ${m} done ERROR" >> /private/docker/complete/$customer/timer
-              else
-                echo "Format ${m} done into $ELAPSED seconds" >> /private/docker/complete/$customer/timer
-              fi
-                fi
-        done
-  done
+    customer="$i-$(echo ${n} | cut -d '_' -f5 | cut -d '.' -f1)-$r"
+    echo "RETURN" >> /private/docker/csv
+    echo $i >> /private/docker/csv
+    for j in ${extensions[@]}
+    do
+      if [ $i != $j ]
+      then
+        echo "Test for ${j}"
+        cp /private/docker/big_buck_bunny_720p_10mb.${i} /private/docker/incomplete/
+        BEFORE=$($DATE +'%s')
+        if [ $r != "nolimit" ]
+        then
+          sudo docker run -it --rm -e INPUT=big_buck_bunny_720p_10mb.${i} -m ${r} -e CUSTOMER=${customer} -e EXTENSION=${j} -v /private/docker:/data amsatique/transcode-worker
+        else
+          sudo docker run -it --rm -e INPUT=big_buck_bunny_720p_10mb.${i} -e CUSTOMER=${customer} -e EXTENSION=${j} -v /private/docker:/data amsatique/transcode-worker
+        fi
+        AFTER=$($DATE +'%s')
+        ELAPSED=$(($AFTER - $BEFORE))
+        if [ $ELAPSED -lt 3 ]
+        then
+          rm /private/docker/complete/${customer}/big_buck_bunny_720p_10mb.${j}
+          echo "Format ${i} to ${j} done ERROR" >> /private/docker/complete/$customer/timer
+          echo "ERROR" >> /private/docker/csv
+        else
+          echo "Format ${i} to ${j} done into $ELAPSED seconds" >> /private/docker/complete/$customer/timer
+          echo $ELAPSED >> /private/docker/csv
+        fi
+      else
+        echo "NONE" >> /private/docker/csv
+      fi
+    done
   done
 done
+sed -e '0,/"RETURN,"/ s/"RETURN,"//' /private/docker/csv && awk -v RS="" '{gsub (/\n/,",")}1' /private/docker/csv > /private/docker/csv2 && awk -v RS="" '{gsub ("RETURN,",/\n/)}1' /private/docker/csv2 > /private/docker/csv3
 exit
